0499bd7b8fe56af5f658d4f5dea23e0ae3039476
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2021-10-01 14:24:29 +0200

    Add files via upload

---
+++ kalkulator_starzyk.py
@@ -0,0 +1,17 @@
+DZIALANIA = int(input('Wybierz dzialanie: 0 - Dodawanie, 1 - Odejmowanie 2 - Mnozenie 3 - Dzielenie'))
+
+if DZIALANIA = 0
+{
+    a = int(input('Wpisz pierwsza liczbe'))
+    b = int(input('Wpisz druga liczbe'))
+    c = a + b
+    print('Wynik dodawania to: %d' & ( c ))
+}
+else if DZIALANIA = 1
+{
+    a = int(input('Wpisz pierwsza liczbe'))
+    b = int(input('Wpisz druga liczbe'))
+    c = a - b
+    print('Wynik odejmowania to: %d' & ( c ))
+}
+else if DZIALANIA = 2
