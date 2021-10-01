aeedadf5d1df1b4a53e4cdac03d92cb802ee4a86
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2021-10-01 15:38:28 +0200

    Update kalkulator_starzyk.py

--- kalkulator_starzyk.py
+++ kalkulator_starzyk.py
@@ -1,35 +1,37 @@
-DZIALANIA = int(input('Wybierz dzialanie: 0 - Dodawanie, 1 - Odejmowanie 2 - Mnozenie 3 - Dzielenie'))
+DZIALANIA = int(input('Wybierz dzialanie: 0 - Dodawanie, 1 - Odejmowanie 2 - Mnozenie 3 - Dzielenie '))
 
-if DZIALANIA = 0
-{
-    a = int(input('Wpisz pierwsza liczbe'))
-    b = int(input('Wpisz druga liczbe'))
+if DZIALANIA == 0:
+
+    a = int(input('Wpisz pierwsza liczbe '))
+    b = int(input('Wpisz druga liczbe '))
     c = a + b
-    print('Wynik dodawania to: %d' & ( c ))
-}
-else if DZIALANIA = 1
-{
-    a = int(input('Wpisz pierwsza liczbe'))
-    b = int(input('Wpisz druga liczbe'))
+    print('Wynik dodawania to: %g' %  c )
+
+if DZIALANIA == 1:
+
+    a = int(input('Wpisz pierwsza liczbe '))
+    b = int(input('Wpisz druga liczbe '))
     c = a - b
-    print('Wynik odejmowania to: %d' & ( c ))
-}
-else if DZIALANIA = 2
-{
-    a = int(input('Wpisz pierwsza liczbe'))
-    b = int(input('Wpisz druga liczbe'))
+    print('Wynik odejmowania to: %g' %  c )
+
+if DZIALANIA == 2:
+
+    a = int(input('Wpisz pierwsza liczbe '))
+    b = int(input('Wpisz druga liczbe '))
     c = a * b
-    print('Wynik mnozenia to: %d' & ( c ))
-else if DZIALANIA = 3
-{
-    a = int(input('Wpisz pierwsza liczbe'))
-    b = int(input('Wpisz druga liczbe'))
-    if b != 0
-    {
+    print('Wynik mnozenia to: %g' %  c )
+if DZIALANIA == 3:
+
+    a = int(input('Wpisz pierwsza liczbe '))
+    b = int(input('Wpisz druga liczbe '))
+    if b != 0:
+    
         c = a * b
-        print('Wynik dzielenia to: %d' & ( c ))
-    }
-    else
-    print('Nie dziel przez zero')
-else
-print('Wprowadzono zla liczbe')
+        print('Wynik dzielenia to: %g' % c )
+    
+    if b == 0:
+    
+        print('Nie dziel przez zero ')
+if DZIALANIA <= 4:
+
+    print('Wprowadzono zla liczbe ')
