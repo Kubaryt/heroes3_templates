15f6e890d3fcaedf61148b6fb58f8dfda09012c6
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2021-10-05 07:43:57 +0200

    Update kalkulator2.py

--- kalkulator2.py
+++ kalkulator2.py
@@ -1,8 +1,9 @@
-DZIALANIA = 5
+import math
+DZIALANIA = 6
 
-while DZIALANIA >= 5:
+while DZIALANIA >= 6:
 
-    DZIALANIA = int(input('Wybierz dzialanie: 0 - Dodawanie | 1 - Odejmowanie | 2 - Mnozenie | 3 - Dzielenie | 4 - Potegowanie '))
+    DZIALANIA = int(input('Wybierz dzialanie: 0 - Dodawanie | 1 - Odejmowanie | 2 - Mnozenie | 3 - Dzielenie | 4 - Potegowanie | 5 - Logarytm naturalny '))
 
 if DZIALANIA == 0:
 
@@ -30,7 +31,7 @@ if DZIALANIA == 3:
     b = 0
     while b == 0:
     
-        b = int(input('Wpisz druga liczbe '))
+        b = float(input('Wpisz druga liczbe '))
     
     c = a / b
     print('Wynik dzielenia to: %f' % c )
@@ -41,3 +42,10 @@ if DZIALANIA == 4:
     b = float(input('Wpisz druga liczbe '))
     c = a ** b
     print('Wynik potegowania to: %f' % c)
+
+if DZIALANIA == 5:
+
+    a = float(input('Wpisz pierwsza liczbe '))
+    c = math.log( a )
+    print('Wynik to: %f' % c)
+
